3554a29f1a9d9bc702de740b681f5991b306139e
paul.liu <paul@nichad.com>
2019-03-07 23:10:53 +0800

    删除了createExcel.py中冉元的邮箱后辍

---
+++ Python/httpclient.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#-------------------------------------------------------------------------------
+# Name:        httpclient
+# Author:      pengfaliu
+# Email:       pengfaliu@163.com	
+# Created:     23/02/2016
+# requirement: python >=2.6
+#-------------------------------------------------------------------------------
+import httplib
+import urllib
+import urllib2
+
